b2dc3261e4d9910cc484ca9a6c04146bf6557504
altadewaal <alta.dewaal@gmail.com>
2025-07-23 11:12:09 +0200

    Created env file

--- vulavula_translation.py
+++ vulavula_translation.py
@@ -1,4 +1,9 @@
-VULAVULA_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMThmNThiMDAtZjViZC00NzE4LTg5NGItMGE2MTNmMjA0OWE2Iiwia2V5Y2hhaW5faWQiOiIwY2NlNzdlZS01NjEyLTQ5MjEtODY5NC1mMTBlNjYyNGM3NDAiLCJwcm9qZWN0X2lkIjoiNWRmNDcyM2QtNTRiYi00ZGIzLWIxOGUtMDk4ZTBkMDNiNzBkIiwiY3JlYXRlZF9hdCI6IjIwMjUtMDctMjFUMTk6Mjk6NDAuMTQzNzgyIn0.9lBDbBAHCL81eqmrNDGW_WqoY1H4gH608-291CglyhA"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+VULAVULA_TOKEN = os.getenv("VULAVULA_TOKEN")
+
 # Endpoint URL
 import requests
 url = 'https://vulavula-services.lelapa.ai/api/v1/translate/process'
